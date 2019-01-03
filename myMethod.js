17eb0b07a20e9f325248c64d29520e368b92a6af
krispthan <44071872+krispthan@users.noreply.github.com>
2019-01-02 18:58:04 -0800

    added new function

--- myMethod.js
+++ myMethod.js
@@ -34,3 +34,12 @@ function randomAlphaArray(arr){
     return arr;
 }
 console.log("Alpha randomizer is ", createRandomAlphaArray);
+//Create an array that iterates forward and backwards
+var array = [1,2,3,4,5];
+
+    for(var i = 0, j = array.length -1; i<array.length,  j>=0 ; i++, j--){
+        console.log("number array at i", array[i]);
+        console.log("number array at j", array[j]);
+    }
+
+
