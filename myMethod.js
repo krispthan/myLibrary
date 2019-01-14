b74cf1b01b352d1579c2879ab64e2366ec69e606
krispthan <44071872+krispthan@users.noreply.github.com>
2019-01-13 21:15:57 -0800

    added stuff

--- myMethod.js
+++ myMethod.js
@@ -85,3 +85,19 @@ function validAnagrams(first,second){
     }
     return true;
 }
+//Bubble Sort 
+function bubbleSort(arr){
+    var doNotSwap;
+    for(var i =arr.length; i >0; i--){
+        doNotSwap= true;
+        for(var j=0; j< i-1; j++){
+           if(arr[j] > arr[j+1]){
+            var temp = arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]= temp;
+            doNotSwap=false;
+           }           
+        }
+        if(doNotSwap) break;  
+    } 
+}
