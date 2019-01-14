be624af4a7a7530ac598dfc2bb796ef476d7e57d
krispthan <44071872+krispthan@users.noreply.github.com>
2019-01-13 19:23:38 -0800

    added another method

--- myMethod.js
+++ myMethod.js
@@ -59,3 +59,29 @@ function stopTimer(){
     $('timer').text('OO');
     clearInterval(timer);
 }
+
+//Anagrams
+function validAnagrams(first,second){
+    if(first.length != second.length){
+        return false;
+    }
+    const lookup ={};
+    for(let i= 0; i<first.length;i++){
+        let letter = first[i];
+        if(lookup[letter]){
+            lookup[letter]++
+        }
+        else{
+            lookup[letter]=1;
+        }
+        for(let i=0; i <second.length; i++){
+            let letter=second[i];
+            if(!lookup[letter]){
+                return false;
+            } else {
+                lookup[letter]-=1;
+            }
+        }
+    }
+    return true;
+}
